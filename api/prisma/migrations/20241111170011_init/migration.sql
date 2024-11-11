5085e23ad434ea2ee5a1ba927bc653637f1cb93e
Adrian Mora <adrian.dario.m@gmail.com>
2024-11-11 12:05:38 -0500

    Add tool to reset prisma db; consolidate migrations

---
+++ api/prisma/migrations/20241111170011_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Application" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "dob" TEXT,
+    "street" TEXT,
+    "city" TEXT,
+    "state" TEXT,
+    "zipcode" TEXT,
+    "vehicles" TEXT
+);
